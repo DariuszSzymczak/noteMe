1f088a74e29f31c4a95d1a4f91b8616990354d0d
Dawid Krakowczyk <35225469+DKrakowczyk@users.noreply.github.com>
2018-05-16 23:49:16 +0200

    reg work

---
+++ accounts/handler.php
@@ -0,0 +1,19 @@
+<?php
+ $register = 'register.php';
+    if(file_exists($register)){
+        require $register;
+        $reg = new register();
+    }
+    else
+    {
+        die('Błąd krytyczny :(');
+    }
+
+    if(isset($_POST['mail']))
+    {   $a = $_POST['mail'];
+        $b = $_POST['login'];
+        $c = $_POST['pass1'];
+        $d = $_POST['pass2'];
+        $reg->addUser($a,$b,$c,$d);
+    }
+?>
