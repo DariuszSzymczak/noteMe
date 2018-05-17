ce9001ec71ab680d9710e6bd14c664969efe82b6
konwasiak <konwasiak@gmail.com>
2018-05-17 15:26:21 +0200

    Div na stronie głównej, poprawione modale logowania i rejestracji

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-        <h3>Zarejestruj się</h3>
+        <h3 class="modal-title">Zarejestruj się</h3>
       </div>
       <div class="modal-body">
       <form action="accounts/handler.php" method="POST">
