a661057b941db59a492d49157362dc209d1b1479
konwasiak <konwasiak@gmail.com>
2018-05-20 15:12:54 +0200

    divide footer, left-sidebar, header, modals HTMLs

---
+++ user_panel/dividedHTML/left-sidebar.php
@@ -0,0 +1,56 @@
+  <!-- Left Sidebar  -->
+  <div class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                        <li class="nav-label">Menu główne</li>
+                        <li>
+                            <a href="userpanel.php">
+                                <i class="fa fa-tachometer"></i>
+                                <span class="hide-menu">Strona główna </a>
+                        </li>
+                        <li>
+                            <a href="#" aria-expanded="false">
+                                <i class="fa fa-envelope"></i>
+                                <span class="hide-menu">Wiadomości</span>
+                            </a>
+                            <li>
+                                <a href="app-profile.php" aria-expanded="false">
+                                    <i class="fa fa-user"></i>
+                                    <span class="hide-menu">Twój profil</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="groups.php" aria-expanded="false">
+                                    <i class="fa fa-table"></i>
+                                    <span class="hide-menu">Grupy</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="tasks.php" aria-expanded="false">
+                                    <i class="fa fa-wpforms"></i>
+                                    <span class="hide-menu">Zadania</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" aria-expanded="false">
+                                    <i class="fa fa-book"></i>
+                                    <span class="hide-menu">Notatki</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" aria-expanded="false">
+                                    <i class="fa fa-level-down"></i>
+                                    <span class="hide-menu">Wyloguj</span>
+                                </a>
+                            </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </div>
+        <!-- End Left Sidebar  -->
