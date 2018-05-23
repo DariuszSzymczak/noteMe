49e0ccee8bc7b15db739c4420aafd2671e5f89e8
Dawid Krakowczyk <35225469+DKrakowczyk@users.noreply.github.com>
2018-05-24 01:24:26 +0200

    nie chce wspolpracowac

--- user_panel/dividedHTML/addPrivateNoteModal.php
+++ user_panel/dividedHTML/addPrivateNoteModal.php
@@ -1,18 +1,18 @@
 <div class="modal" tabindex="-1" role="dialog" aria-labelledby="addPrivateNoteModal" aria-hidden="true" id="addPrivateNoteModal">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <form action="panelClass/panel.php" method="POST" novalidate="novalidate">
+                
                     <div class="modal-header">
                         <h3 class="modal-title">Dodaj notatkę</h3>
                     </div>
                     <div class="modal-body">
                         <div class="">
+                        <form method="POST">
                             <div class="form-group">
                                 <label for="title">  Tytuł </label>
-                                <input type="text" data-val="true" data-val-required="Wpisz tytuł notatki" class="form-control" name="title" id="title"/>
+                                <input type="text" data-val="true" class="form-control" name="title" id="title"/>
                                 <label for="title">Treść:</label>
-                                <textarea name="note-content" style="height: 10em" rows="100" class="form-control" id="note-content"></textarea>
-                                <span class="field-validation-valid text-danger" data-valmsg-for="userName" data-valmsg-replace="true"></span>
+                                <textarea name="note" style="height: 10em" rows="100" class="form-control" id="note-content"></textarea>
                             </div>
                         </div>
 
