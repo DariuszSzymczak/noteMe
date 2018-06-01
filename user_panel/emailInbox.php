d6a90e80ad024c234a7f2b9cabd035077468d2d5
Dawid Krakowczyk <35225469+DKrakowczyk@users.noreply.github.com>
2018-06-01 20:42:54 +0200

    mail sending working

--- user_panel/emailInbox.php
+++ user_panel/emailInbox.php
@@ -2,7 +2,7 @@
     require('dividedHTML/head-section.php');
     require('dividedHTML/header.php');
     require('dividedHTML/left-sidebar.php');
-    $pane->addGroup($pdo,$_SESSION['userID']);
+   
 ?>
 
 <div class="page-wrapper">
@@ -17,6 +17,79 @@
                     </ol>
                 </div>
             </div>
+            
+            <div class="container-fluid">
+            <!-- Start Page Content -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-title">
+                        <h2>Twoje grupy  <?php $pane->sendMail($pdo,$_SESSION['userID']); ?></h2>
+
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nadawca</th>
+                                            <th>Temat</th>
+                                            <th>
+                                                <center>Akcje</center>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                            <br/>
+                            <a href="javascript:;" data-toggle="modal" data-target="#sendMail">
+                                <button style="margin:auto;text-align:center;" type="button" class="btn btn-primary btn-flat btn-addon m-b-10 m-l-5">
+                                    <i class="ti-plane"></i>Wyślij wiadomość</button>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- /# card -->
+                </div>
+                <!-- /# column -->
+            </div>
+            <!-- /# row -->
+            <div class="modal" tabindex="-1" role="dialog" aria-labelledby="sendMail" aria-hidden="true" id="sendMail">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form method="POST" novalidate="novalidate">
+                    <div class="modal-header">
+                        <h3 class="modal-title">Wyślij wiadomość</h3>
+                    </div>
+                    <div class="modal-body">
+                        <div class="">
+                            <div class="form-group">
+                                <label for="groupName">
+                                    Adresat </label>
+                                <input type="text" placeholder="Tutaj wpisz login osoby, z którą chcesz się skontaktować"  class="form-control" name="mailTo" id="groupName"
+                                />
+                                <label for="groupName">
+                                    Temat </label>
+                                <input type="text" placeholder="Temat wiadomości" class="form-control" name="mailTopic" id="mailTopic"/>
+                                <label for="groupSize">
+                                    Treść </label>
+                                <input type="text" placeholder="Treść wiadomości"  class="form-control" name="mailContent" id="mailContent"/>
+                                <span class="field-validation-valid text-danger" data-valmsg-for="groupName" data-valmsg-replace="true"></span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <input type="submit" class="btn btn-primary" value="Wyślij wiadomość"/>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
             
 <?php
     require('dividedHTML/footer.php');
